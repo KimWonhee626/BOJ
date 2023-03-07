078192dd177be1ae48a66058d362dd2ee6eca8fa
KimWonhee626 <69297345+KimWonhee626@users.noreply.github.com>
2023-03-07 17:10:45 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,13 @@
+class Solution{
+    public int solution(int n){
+        int answer = 0;
+        Double sqrt = Math.sqrt(n);
+        if(sqrt == sqrt.intValue()){
+            answer = 1;
+        }
+        else answer = 2;
+
+        return answer;
+
+    }
+}
