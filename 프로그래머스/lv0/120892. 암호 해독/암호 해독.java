1b56da8817def363c171b4abf68304baa4bf3195
KimWonhee626 <69297345+KimWonhee626@users.noreply.github.com>
2023-03-07 18:04:37 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        for(int i = code - 1; i < cipher.length(); i += code){
+            answer.append(cipher.charAt(i));
+        }
+        return answer.toString();
+    }
+}
