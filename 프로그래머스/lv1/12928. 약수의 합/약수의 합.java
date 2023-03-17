0971e22e548891b8a619e8b1d1279ed9aa65a948
KimWonhee626 <69297345+KimWonhee626@users.noreply.github.com>
2023-03-17 13:46:58 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i*i <= n; i++){
+            if(i*i == n){
+                answer += i;
+            }
+            else if(n % i == 0){
+                answer+=i;
+                answer += (n/i);
+            }
+        }
+        return answer;
+    }
+}
